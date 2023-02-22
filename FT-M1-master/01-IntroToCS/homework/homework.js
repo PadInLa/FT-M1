b9a596aa26441cbda8248bc416240a383cf71a0f
Juan David Padilla Paez <53383305+PadInLa@users.noreply.github.com>
2023-02-21 22:24:21 -0500

    First commit
    Hw1 Done

---
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function BinarioADecimal(num) {
+  return parseInt(num, 2);
+}
+
+function DecimalABinario(num) {
+  return num.toString(2);
+}
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+};
